105f02cfffd501113e4c94a83475ed1951bf33f2
shamitomita <shamitomita@gmail.com>
2022-09-25 16:54:41 -0400

    Data types

---
+++ SwiftPRactice/SwiftPractice.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+
+    /*Double: 64-bit floating-point number*/
+var score: Double = 0
+
+let player: String = "Roy"
+let luckyNumber: Int = 13
+let pi:Double = 3.141
+var isEnabled: Bool = true
+
+var albums: [String] = ["Red", "Fearless"]
+var users: [String: String] = ["id":"@twostres"]
+/*Sets contain unique, unordered items*/
+var books: Set<String> = Set(["The Bluest", "Foundation"])
+
+var teams: [String] = [String]()
+var clues = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style: UIStyle = .light
